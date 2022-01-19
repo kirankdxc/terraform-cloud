2874ab12de9c4540ba205ac8b995af2cb3025530
kirankdxc <92788861+kirankdxc@users.noreply.github.com>
2022-01-19 12:15:16 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -9,7 +9,7 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "myec2" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = var.instancetype
+  instance_type = "t2.micro"
   tags = {
     Name = "KK"
   }
